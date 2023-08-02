72c1bcf75bcd8531ffd239d6b68cab8ada887b3f
Sushanta Neupane <sushantaneupane123@gmail.com>
2023-08-02 19:26:29 +0545

    landing page ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,41 +3,37 @@ import { BsArrowRightShort, BsArrowsAngleExpand, BsBackspace, BsBagXFill, BsFace
 
 function Navbar() {
   return (
-    <nav className='flex overflow-hidden flex-wrap justify-between h-[150px] bg-[#22385c] text-[#fefffc] p-5' >
-    <div className="flex-1 brand">
-      <span className='saman text-xl text-neutral-300 font-semibold'>CHITRAKALA</span>
-    </div>
+    <nav className='border-[1px] lg:border-none border-b-[#556f5f] z-1 flex overflow-hidden flex-col gap-[1.5rem] min-h-[200px] h-[100px] bg-[#e5eee9] text-[#232323] p-5' >
+      <div className=" flex justify-between brand">
+        <span className='saman text-xl text-[#556f5f] font-semibold'>CHITRAKALA</span>
+       
+        <div className="handlers text-[#556f5f] pt-0  flex gap-5 text-base ">
+          <li><BsFacebook /></li>
+          <li><BsPinterest /></li>
+          <li><BsInstagram /></li>
+          <li><BsBagXFill /></li>
+          <li><BsArrowRightShort /></li>
+        </div>
 
-
-    <div className=' mt-7  flex flex-1 items-center justify-end h-full flex-col '>
-      <div className="saman logo text-6xl font-extrabold text-center"> CHITRAKALA</div>
-      <div className='link flex  gap-5 m-3 text-neutral-300'>
-        <li>Home</li>
-        <li>Product</li>
-        <li>About</li>
-        <li>Home</li>
-        <li>Product</li>
-        <li>About</li>
-      </div>
-    </div>
-
-    <div className=" flex-1 text-neutral-400 flex-col items-end flex justify-start gap-[3rem] ">
-      <div className="handlers text-neutral-300 pt-0  flex gap-5 text-[12px] ">
-        <li><BsFacebook /></li>
-        <li><BsPinterest /></li>
-        <li><BsInstagram /></li>
-        <li><BsBagXFill/></li>
-        <li><BsArrowRightShort/></li>
-      </div>
-
-      <div className="">
-        <input className='p-2 rounded-sm outline-none' type="text" name="search" id="search" placeholder='Search...' />
       </div>
 
 
-    </div>
+      <div className='  flex  items-center justify-center h-full flex-col '>
+        <div className="saman logo text-6xl font-extrabold text-center"> CHITRAKALA</div>
+        <div className='link flex  gap-5 m-3 text-neutral-300'>
+          <li>Home</li>
+          <li>Product</li>
+          <li>About</li>
+          <li>Artist</li>
 
-  </nav>
+        </div>
+      </div>
+<div className='flex justify-end'>
+
+      <input className='min-w-[100px] p-1 rounded-sm outline-none' type="text" name="search" id="search" placeholder='Search...' />
+</div>
+ 
+    </nav>
   )
 }
 
