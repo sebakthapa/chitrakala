31385621021220c66e3b11dc67c2340a70cbe482
Sushanta Neupane <sushantaneupane123@gmail.com>
2023-10-22 12:41:24 +0545

    navbar and other cards ui cahnged

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -1,6 +1,8 @@
 "use client"
 import { useState, useEffect } from 'react'
 import Image from 'next/image'
+import TopCreator from './TopCreator'
+import { motion } from 'framer-motion'
 const Artist = () => {
 
   const [users, setUsers] = useState([])
@@ -19,13 +21,70 @@ const Artist = () => {
   return (
     <>
 
-
   
 
     <div className='m-5 '>
         <button className='border text-[#556f5f] border-[#556f5f] hover:bg-[#556f5f] hover:text-white py-2 px-6 rounded-full'>Filter</button>
     </div>
-    <main className='bg-[#gagaga] m-5 flex flex-wrap  gap-7 '>
+
+
+    <main class="py-4">
+        <div class="px-4">
+          <div class="block md:flex  md:-mx-2">
+            {users.map((item, index) => (
+              <div key={index} class="w-full lg:w-1/4 md:mx-2 mb-4 md:mb-0">
+                <div class="bg-white rounded-lg overflow-hidden shadow relative">
+                  <div className=" h-[30vh] overflow-hidden  ">
+                    <motion.img
+                      key={index}
+                      src="/landing/oil.jpg"
+                      whileHover={{ scale: 1.1 }}
+                      whileTap={{ scale: 0.9 }}
+                      transition={{
+                        type: "spring",
+                        stiffness: 200,
+                        damping: 10,
+                      }}
+                    />
+
+                    <div className="pp flex-initial overflow-hidden border-white border-[2px] top-1 bg-black text-white w-12 text-center h-12 m-1 rounded-full absolute bottom-0">
+                      <img src={item.photo || "/avatar.png"} width={50} height={50} />
+                    </div>
+                  </div>
+
+                  <div class="p-4 h-auto md:h-40 lg:h-48">
+                    <a
+                      href="#"
+                      class="block text-blue-500 hover:text-blue-600 font-semibold mb-2 text-lg md:text-base lg:text-lg"
+                    >
+                      {item.displayName}
+                    </a>
+                    <div class="text-gray-600 text-sm leading-relaxed block md:text-xs lg:text-sm">
+                      {item.bio}
+                    </div>
+                    <div class="relative mt-2 lg:absolute bottom-0 mb-4 md:hidden lg:block">
+                      <a
+                        class="inline bg-gray-300 py-1 px-2 rounded-full text-xs lowercase text-gray-700"
+                        href="#"
+                      >
+                     {item.address}
+                      </a>
+                      <a
+                        class="inline bg-gray-300 py-1 px-2 rounded-full text-xs lowercase text-gray-700"
+                        href="#"
+                      >
+                      {item.dob?.split('T')[0]}
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </main>
+      
+    {/* <main className='bg-[#gagaga] m-5 flex flex-col overflow-hidden  gap-7 '>
     {users.map((user,index) => (
 
     
@@ -42,7 +101,8 @@ const Artist = () => {
             <div>{user.displayName}</div>
             <div>{user.bio}</div>
             <div>{user.address}</div>
-            <div>{ user.dob}</div>
+            
+            <div>{ user.dob?.split('T')[0]}</div>
     
           </div>
         </aside>
@@ -55,7 +115,7 @@ const Artist = () => {
       </section>
 
 ))}
-</main>
+</main> */}
     </>
   )
 }
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -7,12 +7,17 @@ function Landing() {
         <>
            
 
-            <div className='flex  overflow-hidden  bg-[#fff] min-h-[50vh] h-[70vh]'>
+            <div className='flex  overflow-hidden  bg-[#fff] min-h-[50vh] h-[90vh]'>
 
                 <div className="flex-[100%]  mt-20 flex items-center flex-col justify-center">
-                    <span className='text-7xl p-5  font-extrabold'>The Art</span>
-                    <span className='text-2xl text-center pb-3  font-extralight'>This is Marketplace & Gallery of Excleusive painting</span>
-                    <span className='text-xl  text-center  font-extralight'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi repellat distinctio eligendi itaque illum. Inventore iste sunt perferendis hic,</span>
+                    <span className='text-7xl saman p-5  font-extrabold'>Chitrakala</span>
+                    <p className="block font-sans text-2xl text-center font-light leading-relaxed text-gray-700 antialiased">
+                    The Marketplace & Gallery of Excleusive painting
+          </p>
+                    
+                    <span className='text-xl  text-center  font-extralight'> 
+                    
+                    </span>
                     <div className='flex gap-5 m-10'>
                         <button className='border text-[#556f5f] border-[#556f5f] hover:bg-[#556f5f] hover:text-white py-2 px-6 rounded-full'>Explore</button>
                         <button className='border text-[#556f5f] border-[#556f5f] hover:bg-[#556f5f] hover:text-white py-2 px-6 rounded-full'>Shop</button>
