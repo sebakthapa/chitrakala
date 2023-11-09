1be48a9a7dfecdf7a20f5b92d42729f4010bffbe
Sushanta Neupane <sushantaneupane123@gmail.com>
2023-11-09 15:31:46 +0545

    minor ui

--- src/app/gallery/components/Gallery.jsx
+++ src/app/gallery/components/Gallery.jsx
@@ -22,19 +22,20 @@ const Gallery = () => {
     if (category) { query += `&category=${category}`; }
     if (limit) { query += `&limit=${limit}`; }
 
-    const dispatch = useDispatch();
-    const galleryData = useSelector(state => state.gallery);
     const [filterParams, setFilterParams] = useState(filter||"newD");
     const [filteredData, setFilteredData] = useState([]);
     const [sortedData, setSortedData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [loading1, setLoading1] = useState(true);
 
     async function fetchData() {
         try {
             const res = await axios.get(`/api/products?${query}`);
             if (res.status === 200) {
                 // dispatch(addGalleryData(res.data));
-                setFilteredData(res.data)
                 
+                setFilteredData(res.data)
+                setLoading(false)
             }
            
         }
@@ -56,13 +57,13 @@ const Gallery = () => {
 
       async function fetchSortedData() {
         try {
+          setLoading1(true)
             const res = await axios.get(`/api/products?filter=${filterParams}`);
             
             if (res.status === 200) {
-                // dispatch(addGalleryData(res.data));
-                // setSortedData(res.data)
-                setSortedData(null)
+              
                 setSortedData(res.data)
+                setLoading1(false)
                 
                 
             }
@@ -79,13 +80,17 @@ const Gallery = () => {
 
     return (
         <>
-          {filteredData.length>0 ? (
-            <div className="myScroll pb-10 flex overflow-x-auto">
-              {filteredData?.map((item, index) => <ArtCard key={index} item={item} />)}
-            </div>
-          ) : (
-            <Skeleton  containerClassName="h-[2rem] w-full" count={3} />
-          )}
+
+{loading ? (
+                <div className="myScroll pb-10 flex overflow-x-auto">
+                    <Skeleton count={5} />
+                </div>
+            ) : (
+                <div className="myScroll pb-10 flex overflow-x-auto">
+                    {filteredData?.map((item, index) => <ArtCard key={index} item={item} />)}
+                </div>
+            )}
+   
           <h2 className="mb-4 text-center text-2xl text-gray-900 font-bold md:text-4xl">Explore</h2>
       
           <label htmlFor="filter" className="text-gray-400 font-bold text-xs">Sort</label>
@@ -96,15 +101,12 @@ const Gallery = () => {
               <option className="text-gray-700 block px-4 py-2 text-sm " value={"priceD"}>Price</option>
             </select>
           </div>
-      
-          {sortedData.length>0 ? (
+
             <div className="myScroll pb-10 flex overflow-x-auto">
                     {sortedData?.map((item,index) => <ArtCard key={index} item={item} />)  }
 
             </div>
-          ) : (
-            <Skeleton containerClassName="h-[2rem] w-full" count={3} />
-          )}
+  
         </>
       );
       
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,12 +8,7 @@ export const metadata = {
 };
 const Page = () => {
 
-  const images = [
-    "https://img.freepik.com/premium-photo/spectacular-abstract-city-orange-teal-digital-art-3d-illustration_31965-49704.jpg",
-    "https://images.nightcafe.studio/jobs/cNxmQICjMo8xdPi31Zjt/cNxmQICjMo8xdPi31Zjt_6.9444x.jpg?tr=w-9999,c-at_max",
-    "https://cdn11.bigcommerce.com/s-x49po/images/stencil/1500x1500/products/43339/58776/1571390056201_6D1F1BD8-CA91-4B86-AD0F-249681E867B0__71341.1571633277.jpg?c=2",
-    "https://i.pinimg.com/736x/60/d3/6f/60d36f7dec5f9a2818a08b964def0190.jpg",
-  ];
+
    
 
   return (
@@ -21,19 +16,19 @@ const Page = () => {
       <Landing />
    
 
-      <section className=" bg-slate-100 categoriesSection  md:my-36 md:px-5 md:pb-36">
+      <section className=" bg-slate-100 categoriesSection  md:py-36 md:px-5 md:pb-36">
         <h1 className="font-semibold font-poppins text-3xl p-10 text-center mb-5">Explore by Category<span className="font-semibold font-sans text-5xl animate_blink">_</span></h1>
-    <CategoryCard/>
+         <CategoryCard/>
       </section>
 
-
+   
 
 
    
 
-      <div className="bg-gray-200 font-sans h-screen w-full flex flex-row justify-center items-center">
-      <div className="w-full">
-             <MiniCarousel images={images} />
+      <div className=" font-sans h-screen w-full flex flex-row justify-center items-center">
+      <div className="w-full flex justify-center">
+             <MiniCarousel />
 
           </div>
       </div>
