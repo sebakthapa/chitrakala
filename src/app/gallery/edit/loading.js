199453e625654bb1ca8313c49128e6c069fc3963
Sushanta Neupane <sushantaneupane123@gmail.com>
2023-11-10 22:28:25 +0545

    artwork delete and edit

---
+++ src/app/gallery/edit/loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import LoadingComponent from '@/components/LoadingComponent'
+const Loading = () => {
+  return (
+    <LoadingComponent/>
+  )
+}
+
+export default Loading
