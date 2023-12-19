1acd2b5050e0cd1e18c9243ed8905e718ab5d9dd
SebakThapa <thapasebak059@gmail.com>
2023-12-19 23:43:38 +0545

    new card ui for gallery

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -30,3 +30,23 @@ export const typingEffect = (string, setState, option) => {
 }
 
 
+var unitlist = ["", "k", "m", "b", "t"];
+export function formatNumberWithLetter(number) {
+    let sign = Math.sign(number);
+    let unit = 0;
+
+    while (Math.abs(number) >= 1000) {
+        unit = unit + 1;
+        number = Math.floor(Math.abs(number) / 100) / 10;
+    }
+    return sign * Math.abs(number) + unitlist[unit];
+}
+
+
+export const categoriesColor = {
+        oil: "#D4AB9C",
+        water: "#B3E1EB",
+        digital: "#9AAAC5",
+        sketch: "#DCDCDC",
+        more: "#C9B5A5",
+}
