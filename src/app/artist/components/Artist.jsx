100c3f02cc1e316f5e99a7ae5176fe59125b49e0
Sushanta Neupane <sushantaneupane123@gmail.com>
2024-01-01 12:38:23 +0545

    ui for mobile

--- src/app/artist/components/Artist.jsx
+++ src/app/artist/components/Artist.jsx
@@ -23,11 +23,30 @@ const Artist = () => {
     <>
       <main className=" bg-white m-5 p-10">
         
+      <div class="searchBar flex  items-center max-w-md my-5 bg-white rounded-full border border-gray-800 " x-data="{ search: '' }">
+        <div class="w-full">
+            <input type="search" class="w-full px-4 py-1 text-gray-800 rounded-full focus:outline-none"
+                placeholder="Search" x-model="search"/>
+        </div>
+        <div>
+            <button type="submit" class="flex items-center  justify-center w-12 h-12 text-gray-800 rounded-r-lg">
+                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                </svg>
+            </button>
+        </div>
+      </div>
+
+        
           <div className="block md:flex  md:-mx-2">
             {users?.length > 0 && users.map((item, index) => {
               if (item.artWorks.length == 0) return;
 
-              return (<div key={index} className="w-full lg:w-1/4 md:mx-2 mb-4 md:mb-0">
+              return (
+                
+              <div key={index} className="w-full min-w-[300px] bg-white rounded-lg  darkk:bg-gray-800 darkk:border-gray-700">
                 <Link href="/artists/[id]" as={`/artist/${item?.user?._id}`}>
                   <div className="bg-white rounded-lg overflow-hidden shadow relative">
                     <div className=" h-[30vh] overflow-hidden  ">
@@ -90,3 +109,67 @@ const Artist = () => {
 }
 
 export default Artist
+
+
+{/* <div className="w-full min-w-[300px] bg-white rounded-lg  darkk:bg-gray-800 darkk:border-gray-700">
+<Link href={`/arts/${artData?._id}`} className="block w-full aspect-[5/4] relative group">
+    <Image height={400} width={400} className=" rounded-lg object-cover w-full h-full" src={artData?.photo} alt="art image" />
+    <div className="texts  flex items-end pb-4 px-3 justify-between absolute w-full bottom-0 left-0 h-[100px] from-transparent to-[rgba(0,0,0,.6)] from-0% bg-gradient-to-b text-gray-100 font-medium opacity-0 group-hover:opacity-100 rounded-lg transition duration-300">
+        <p ref={artNameRef} className="title w-full flex-1 line-clamp-1">{artData?.name} &nbsp;</p>
+
+        <span className="text-sm w-fit font-semibold text-gray-100 darkk:text-white">
+            {
+                artData?.price ? `Rs ${formatNumberWithLetter(artData?.price)}` : <span className=" bg-lime-600  p-1 px-2 rounded-full text-xs">Showcase</span>
+            }
+            {artData.category}
+
+        </span>
+
+
+    </div>
+</Link>
+<div className="px-1 pb-5 my-2.5">
+    <div className="head flex justify-between items-center">
+        <Link href={`/artist/%${artData?.artist?._id}`} className="flex items-center gap-3 w-full flex-1">
+            <Image title={artData?.artist?.name} width={30} height={30} alt="artist image" src={artData?.artist?.image} className="rounded-full aspect-square object-cover" />
+            <h6 ref={artistNameRef} className="font-medium  w-full flex-1 h-fulltracking-tight text-gray-700 darkk:text-white capitalize">
+                {artData?.artist?.name.toLowerCase()}
+                &nbsp;
+            </h6>
+        </Link>
+
+        <div className="flex items-center">
+            <div
+                className="flex items-center  rtl:space-x-reverse cursor-pointer "
+                onClick={() => {
+                    toggleLike(artData?._id);
+                }}>
+                {
+                    !checkLiked(artData?.likes, user?._id) ? <BsHeart fontSize={"1.2rem"} fill="gray" /> :
+                        <motion.span
+                            className="block"
+                            initial={{ opacity: 0, scale: 0.7 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            transition={{
+                                duration: 0.3,
+                                ease: [0, 0.71, 0.2, 1.01],
+                                scale: {
+                                    type: "spring",
+                                    damping: 5,
+                                    stiffness: 100,
+                                    restDelta: 0.001
+                                }
+                            }}
+                        >
+                            <BsHeartFill fontSize={"1.2rem"} fill="#ed495b" />
+                        </motion.span>}
+
+            </div>
+            <span className="text-[.9rem] text-gray-600  font-semibold  darkk:bg-blue-200 darkk:text-blue-800 mx-2 select-none">
+
+                {formatNumberWithLetter(artData?.likes?.length) || 0}
+            </span>
+        </div>
+    </div>
+</div>
+</div > */}
